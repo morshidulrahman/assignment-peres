c0d06846127f43547591d67b47b4e913a86bce28
younusFoysal <younus.faysal@gmail.com>
2024-11-16 04:37:00 +0600

    carousel added

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css';
 import Header from './component/Header';
+import CourseCards from "./component/CourseCards.jsx";
 
 function App() {
   return (
     <>
       <Header />
+        <CourseCards/>
     </>
   );
 }
